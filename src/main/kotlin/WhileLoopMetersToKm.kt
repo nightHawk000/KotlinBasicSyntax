b06e89ee754eed6186dc406d0b5ddb7788d5eb06
David Maina <david.kariu6@gmail.com>
2025-07-29 14:57:13 +0300

    pull to local device (29.07.2025)
    Just a test pull

--- src/main/kotlin/WhileLoopMetersToKm.kt
+++ src/main/kotlin/WhileLoopMetersToKm.kt
@@ -1,12 +1,10 @@
-//start
-
 fun main() {
     val meters = listOf(500, 5550, 350, 200, 660)
    
     var index = 0
     
     while(index!= meters.size){
-        val kilometers = meters[index].toDouble()/1000
+        var kilometers = meters[index].toDouble()/1000
         
         println("${meters[index]} m is equal to $kilometers km.")
         index++
