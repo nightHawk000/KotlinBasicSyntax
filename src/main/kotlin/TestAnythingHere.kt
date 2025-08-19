5115fa3dde6a1fb416120aeacbeaac0a776e18dd
David Maina <david.kariu6@gmail.com>
2025-08-19 14:02:49 +0300

    Commit 19.08.2025 @14.02

--- src/main/kotlin/TestAnythingHere.kt
+++ src/main/kotlin/TestAnythingHere.kt
@@ -1,6 +1,17 @@
+fun add (){
+    val a = 5
+    val x = 2
+
+    println("The sum of $a and $x is ${a+x}!")
+
+}
+
+
 fun main(){
  //
     val name = "Dave"
     println("Hello World, my name is ${name.uppercase()}!")
+
+    add()
 
 }
